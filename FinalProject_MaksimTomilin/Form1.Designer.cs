555fed8d4af81d46ced8a2c618bd28ae872e75f9
diduk <diduk@DESKTOP-A3GN9SI>
2022-12-12 15:15:17 +0200

    Добавьте файлы проекта.

---
+++ FinalProject_MaksimTomilin/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace FinalProject_MaksimTomilin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 262);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelQuestion.Location = new System.Drawing.Point(12, 277);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(776, 79);
+            this.labelQuestion.TabIndex = 1;
+            this.labelQuestion.Text = "Objective";
+            this.labelQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(380, 63);
+            this.button1.TabIndex = 2;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.verifyButton);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(408, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(380, 63);
+            this.button2.TabIndex = 3;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.verifyButton);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(12, 428);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(380, 63);
+            this.button3.TabIndex = 4;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.verifyButton);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(408, 428);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(380, 63);
+            this.button4.TabIndex = 5;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.verifyButton);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exitButton.Location = new System.Drawing.Point(362, 497);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 43);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "EXIT";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 544);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelQuestion);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Countries Quiz Game";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ FinalProject_MaksimTomilin/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject_MaksimTomilin
+{
+    public partial class Form1 : Form
+    {
+        int rightNumbers;
+        int objectiveNum = 1;
+        int scoreCount;
+        int allPercentage;
+        int totalScore;
+        public Form1()
+        {
+            List<int> objectiveNums = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            InitializeComponent();
+            objective(objectiveNum);
+            totalScore = 10;
+            string message = "Welcome to Countries Quiz Game! " + "There will be 10 questions related to countries and their capitals, at the end a counter of correct answers and a percentage will be displayed, good luck!";
+            MessageBox.Show(message);
+        }
+
+        private void verifyButton(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+            if (buttonTag == rightNumbers)
+            {
+                scoreCount++;
+            }
+            
+            if(objectiveNum == totalScore)
+            {
+                allPercentage = (int)Math.Round((double)(scoreCount * 100) / totalScore);
+                DialogResult dialogResult = MessageBox.Show("You have completed the quiz. " + "You answered correctly " + scoreCount + " questions. " +
+                    "The total number of correct answers is exactly " 
+                    + allPercentage + "%. " + "If you would like to start over, click *Yes* button.","Results", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                   
+                }
+                else
+                {
+                    Application.Exit();
+                }
+                scoreCount = 0;
+                objectiveNum = 0;
+                objective(objectiveNum);
+            }
+            objectiveNum++;
+            objective(objectiveNum);
+        }
+        private void objective(int qnum) 
+        {
+            switch(qnum)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.FlagOfEstonia;
+                    labelQuestion.Text = "Name the flag of the country";
+                    button1.Text = "Pljevlja, Serbia";
+                    button3.Text = "Finland";
+                    button2.Text = "Argentina";
+                    button4.Text = "Estonia";
+                    rightNumbers = 4;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.FlagOfPeru;
+                    labelQuestion.Text = "Name the flag of the country";
+                    button1.Text = "Indonesia";
+                    button3.Text = "Monaco";
+                    button2.Text = "Peru";
+                    button4.Text = "Norway";
+                    rightNumbers = 2;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.FlagOfUruguay;
+                    labelQuestion.Text = "Name the flag of the country";
+                    button1.Text = "Uruguay";
+                    button3.Text = "Chile";
+                    button2.Text = "Colombia";
+                    button4.Text = "Ecuador";
+                    rightNumbers = 1;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.Madrid;
+                    labelQuestion.Text = "Name the country of this capital - Madrid";
+                    button1.Text = "Spain";
+                    button3.Text = "Portugal";
+                    button2.Text = "France";
+                    button4.Text = "Italy";
+                    rightNumbers = 1;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources.FlagOfAustria;
+                    labelQuestion.Text = "Name the flag of the country";
+                    button1.Text = "Singapore";
+                    button3.Text = "Austria";
+                    button2.Text = "Hungary";
+                    button4.Text = "Canada";
+                    rightNumbers = 3;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources.FlagOfIndonesia;
+                    labelQuestion.Text = "Name the flag of the country";
+                    button1.Text = "Poland";
+                    button3.Text = "Denmark";
+                    button2.Text = "Switzerland";
+                    button4.Text = "Indonesia";
+                    rightNumbers = 4;
+                    break;
+                case 7:
+                    pictureBox1.Image = Properties.Resources.Stockholm;
+                    labelQuestion.Text = "Name the country of this capital - Stockholm";
+                    button1.Text = "Norway";
+                    button3.Text = "Iceland";
+                    button2.Text = "Sweden";
+                    button4.Text = "Finland";
+                    rightNumbers = 2;
+                    break;
+                case 8:
+                    pictureBox1.Image = Properties.Resources.FlagOfThePhilippines;
+                    labelQuestion.Text = "Name the flag of the country";
+                    button1.Text = "Malaysia";
+                    button3.Text = "Czech Republic";
+                    button2.Text = "Puerto Rico";
+                    button4.Text = "Philippines";
+                    rightNumbers = 4;
+                    break;
+                case 9:
+                    pictureBox1.Image = Properties.Resources.Beijing;
+                    labelQuestion.Text = "Name the country of this capital - Beijing";
+                    button1.Text = "Turkey";
+                    button3.Text = "China";
+                    button2.Text = "Vietnam";
+                    button4.Text = "Morocco";
+                    rightNumbers = 3;
+                    break;
+                case 10:
+                    pictureBox1.Image = Properties.Resources.FlagOfFinland;
+                    labelQuestion.Text = "Name the flag of the country";
+                    button1.Text = "Finland";
+                    button3.Text = "Latvia";
+                    button2.Text = "Norway";
+                    button4.Text = "Iceland";
+                    rightNumbers = 1;
+                    break;
+
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
